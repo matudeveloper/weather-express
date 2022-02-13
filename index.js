956c8ec19cc761eea5cb94435ba3e088edcadd7c
Matu developer <martin.laan@voco.ee>
2022-02-13 15:18:34 +0200

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ app.set('views', path.join( __dirname, 'views'))
 const key = '77f97b598769bf8861004c74e30ebbba';
 let city = 'Tartu'
 
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(responce => {
+                return responce.json()
+            })
+            .then((data) => {
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp)-273.15)
+                let result = {
+                    description: description,
+                    city: city,
+                    temp: temp
+                }
+                resolve(result)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+app.all('/', function (req, res){
+    let city
+    if(req.method == 'GET'){
+        city = "Tartu"
+    }
+    if(req.method == 'POST'){
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data => {
+            res.render('index', data)
+        })
+})
+/*
 app.get('/', function (req, res){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then((responce) => {
@@ -53,4 +91,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 })
 
 })
+
+
+ */
 app.listen(3000)
