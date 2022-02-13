4d97d4ec58aa6ece2c9362519c5e45029cecdcb4
Matu developer <martin.laan@voco.ee>
2022-02-13 14:38:26 +0200

    Added API data to project template file

--- index.js
+++ index.js
@@ -11,25 +11,46 @@ app.set('views', path.join( __dirname, 'views'))
 const key = '77f97b598769bf8861004c74e30ebbba';
 let city = 'Tartu'
 
-app.get('/', function(reg, res){
+app.get('/', function (req, res){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-        .then((response) => {
-        return response.json()
-    })
+        .then((responce) => {
+            return responce.json()
+        })
         .then((data) => {
             let description = data.weather[0].description
             let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp) -273.15)
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
             console.log(description)
             console.log(city)
             console.log(temp)
+
             res.render('index', {
                 description: description,
                 city: city,
                 temp: temp
+                })
             })
-        })
-
 })
 
+app.post('/', function (req, res){
+console.log(req.body)
+//res.redirect('/')
+
+let city = req.body.cityname
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+.then((responce) => {
+    return responce.json()
+})
+.then((data) => {
+    let description = data.weather[0].description
+    let city = data.name
+    let temp = Math.round(parseFloat(data.main.temp)-273.15)
+    res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+    })
+})
+
+})
 app.listen(3000)
